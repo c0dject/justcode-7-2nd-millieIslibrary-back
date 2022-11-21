9934a44918d101852f0fe6e2836c841356ce0598
sangyun lee <sororiri@gmail.com>
2022-11-21 11:50:49 +0900

    [justcode 이상윤] 서적 찾기 API 내  내서재 가져오는 옵션 추가 - showMyBook 라는 옵션 추가 (ture로 할시 자기 계정의 내서재를 가져옴)

--- models/book.dao.js
+++ models/book.dao.js
@@ -63,19 +63,37 @@ const createBookAuthor = async (booksId, authorsId) => {
   return insertStatus;
 };
 
-const findBooks = async (serchOption) => {
-  const { categoriesId, authorsId, authorName, booksId, categoryName, limit, order } =
-    serchOption;
+const findBooks = async serchOption => {
+  let {
+    categoriesId,
+    authorsId,
+    authorName,
+    booksId,
+    categoryName,
+    publisher,
+    limit,
+    order,
+    usersId,
+    showMyBooks,
+  } = serchOption;
+
+  if (!Number.isInteger(limit)) {
+    limit = undefined;
+  }
+
   const NOT_NULL_STATE = 'IS NOT NULL';
   const categoriesIdState = categoriesId  ? `= '${categoriesId}'` : NOT_NULL_STATE;
   const authorsIdState    = authorsId     ? `= '${authorsId}'`    : NOT_NULL_STATE;
   const authorNameState   = authorName    ? `= '${authorName}'`   : NOT_NULL_STATE;
   const booksIdStateState = booksId       ? `= '${booksId}'`      : NOT_NULL_STATE;
   const categoryNameState = categoryName  ? `= '${categoryName}'` : NOT_NULL_STATE;
+  const publisherState    = publisher     ? `= '${publisher}'`    : NOT_NULL_STATE;
+  const userIdState       = usersId       ? `= '${usersId}'`      : NOT_NULL_STATE;
   const limitState        = limit         ? `LIMIT ${limit}`      : '';
   const orderPairMap = {
     rating: 'rating_score',
     page: 'page',
+    publishTime: 'publish_time',
   };
   const orderStateItem =
     order &&
@@ -111,6 +129,13 @@ const findBooks = async (serchOption) => {
           ON authors.id = books_authors.authors_id
         JOIN categories
           ON categories.id = books.categories_id
+        ${
+          showMyBooks
+            ? `
+        JOIN bookshelves
+          ON bookshelves.books_id = books.id`
+            : ''
+        }
       WHERE
           books.id ${booksIdStateState}
         AND
@@ -121,11 +146,21 @@ const findBooks = async (serchOption) => {
           authors.author_name ${authorNameState}
         AND
           categories.content ${categoryNameState}
+        AND
+          books.publisher ${publisherState}
+        ${
+          showMyBooks
+            ? `
+        AND
+          bookshelves.users_id ${userIdState}`
+            : ''
+        }
       ${orderState} -- ORDER BY ...
       ${limitState} -- LIMIT 10
     `,
     [booksId, categoriesId, authorsId, authorName]
   );
+  console.log("found book's length: ", foundBooks.length);
   if (booksId) {
     return foundBooks[0];
   }
